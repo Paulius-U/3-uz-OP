cdadb7098c4d8c6701afb002205656b997b1822f
Paulius-U <75576100+Paulius-U@users.noreply.github.com>
2021-05-08 19:53:45 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,47 +1,35 @@
 #include <iostream> 
-#include <chrono>
 
-#include "failas.h"
-#include "funkcijos.h"
+#include "failas.cpp"
+#include "funkcijos.cpp"
+
 
 int main()
 {
-    char konteineris;
-    std::cout << "Pasirinkite konteineri: " << std::endl;
-    std::cout << "vector:   (0)" << std::endl;
-    std::cout << "list      (1)" << std::endl;
-    std::cout << "deque     (2)" << std::endl;
-    std::cout << "Pasirinkimas: ";
-    std::cin >> konteineris;
-    Pasirinkimas (konteineris, '0', '1', '2');
+    // char failas;
+    // std::cout << "Sugeneruoti failus kompiuterui: (1/0) ";
+    // std::cin >> failas;
+    // Pasirinkimas (failas, '0', '1');
 
-    for(int i = 0; i < 3; i++)
-        std::cout << std::endl;
+    // for(int i = 0; i < 3; i++)
+    //     std::cout << std::endl;
 
-    char failas;
-    std::cout << "Sugeneruoti failus kompiuterui: (1/0) ";
-    std::cin >> failas;
-    Pasirinkimas (failas, '0', '1');
-
-    for(int i = 0; i < 3; i++)
-        std::cout << std::endl;
-
-    if (failas == '1')
-    {
-        int kiek = 1000;
-        std::cout << "Failu sukurimas uztruko: " << std::endl;
-        for(int i = 0; i < 4; i++)
-        {
-            auto Start = std::chrono::high_resolution_clock::now();
-            Sukurimas(kiek);
-            std::cout << "kursiokai" << kiek << ".txt uztruko: ";
-            kiek = kiek * 10;
-            auto End = std::chrono::high_resolution_clock::now();
-            std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(End - Start).count() / 1000.<< " s."<< std::endl;
-        }
-    }
-    for(int i = 0; i < 3; i++)
-        std::cout << std::endl;
+    // if (failas == '1')
+    // {
+    //     int kiek = 1000;
+    //     std::cout << "Failu sukurimas uztruko: " << std::endl;
+    //     for(int i = 0; i < 4; i++)
+    //     {
+    //         auto Start = std::chrono::high_resolution_clock::now();
+    //         Sukurimas(kiek);
+    //         std::cout << "kursiokai" << kiek << ".txt - ";
+    //         kiek = kiek * 10;
+    //         auto End = std::chrono::high_resolution_clock::now();
+    //         std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(End - Start).count() / 1000.<< " s."<< std::endl;
+    //     }
+    // }
+    // for(int i = 0; i < 3; i++)
+    //     std::cout << std::endl;
 
     // char Data;
     // std::cout << "Kaip viskas vyks: " << std::endl;
@@ -66,9 +54,6 @@ int main()
 
     // for(int i = 0; i < 3; i++)
     //     std::cout << std::endl;
-
-    if (konteineris == '0') Vectorius('0', '1');
-    if (konteineris == '1') Listas('0', '1');
-    if (konteineris == '2') Dequelas('0', '1');
+    Vectorius('0','1');
     return 0;
 }
